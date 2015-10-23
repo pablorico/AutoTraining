b3af17fa6f5274fe024f5d41d86e6af1a43b6505
Pablo E. Rico <pablo.e.rico@gmail.com>
2015-10-22 21:21:47 -0300

    Scratch

---
+++ BasicJPA/src/main/java/autotraining/basicjpa/BasicJpaApplication.java
@@ -0,0 +1,12 @@
+package autotraining.basicjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicJpaApplication.class, args);
+    }
+}
