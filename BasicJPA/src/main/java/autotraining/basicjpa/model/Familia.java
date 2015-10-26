3f9a7b2acde15d42d97c898e8b62f79b63134eb7
Pablo E. Rico <pablo.e.rico@gmail.com>
2015-10-25 23:28:43 -0300

    Clean Entities Finally Working.

--- BasicJPA/src/main/java/autotraining/basicjpa/model/Familia.java
+++ BasicJPA/src/main/java/autotraining/basicjpa/model/Familia.java
@@ -9,10 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.Version;
-
-import org.hibernate.annotations.Cascade;
 
 import javax.persistence.OneToMany;
 
