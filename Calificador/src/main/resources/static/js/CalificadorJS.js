3f55b327125b0e0fa948e90acb111313a07c29e5
Pablo E. Rico <pablo.e.rico@gmail.com>
2016-03-13 21:14:37 -0300

    Still trying sound

--- Calificador/src/main/resources/static/js/CalificadorJS.js
+++ Calificador/src/main/resources/static/js/CalificadorJS.js
@@ -1,41 +1,5 @@
-function ping() {
-}
-
-function autoCloseGraciasModal() {
-	setTimeout(hideGraciasModal, 2000);
-}
-
-function hideGraciasModal() {
-	/* $('#gracias').modal('hide'); */
-	window.location.replace("/gracias");
-}
-
-function shot(event) {
-	var button = $(event.relatedTarget);
-	var origin = button.data("whatever");
-	$("#main").load(s);
-}
-
-function bootstrapSoundLoad() {
-
-	ion.sound({
-		sounds : [ {
-			name : "beer_can_opening"
-		}, {
-			name : "bell_ring"
-		}, {
-			name : "branch_break"
-		}, {
-			name : "button_click"
-		} ],
-
-		path : "sounds/",
-		preload : true,
-		multiplay : true,
-		volume : 0.9
-	});
-}
-
 function playButtonClickSound() {
-	ion.sound.play("button_click");
+	var audio = document.getElementsByTagName("audio")[0];
+	audio.load();
+	audio.play();
 }
