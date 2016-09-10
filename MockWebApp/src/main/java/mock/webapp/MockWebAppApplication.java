55498256b8f74541c799e29f96ac407aa27ff999
Pablo E. Rico <pablo.e.rico@gmail.com>
2016-09-10 14:13:23 -0300

    Scratch from SpringIntlzr

---
+++ MockWebApp/src/main/java/mock/webapp/MockWebAppApplication.java
@@ -0,0 +1,12 @@
+package mock.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockWebAppApplication.class, args);
+	}
+}
